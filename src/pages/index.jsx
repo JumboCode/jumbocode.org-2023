b1774dc852b69ae90961066c6a137397329606e2
Luke Taylor <luke@deentaylor.com>
2022-02-15 02:14:31 -0500

    Scrollable homepage content demonstrating text styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,249 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Hello, world!</div>;
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <h2>Hello, World!</h2>
+      <h3>Hello, World!</h3>
+      <h4>Hello, World!</h4>
+      <h5>Hello, World!</h5>
+      <h6>Hello, World!</h6>
+      <p>Hello, World!</p>
+      <p style={{ marginTop: '0.5em' }}>
+        The sun was shining on the sea,
+        <br />
+        Shining with all his might:
+        <br />
+        He did his very best to make
+        <br />
+        The billows smooth and bright —
+        <br />
+        And this was odd, because it was
+        <br />
+        The middle of the night.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        The moon was shining sulkily,
+        <br />
+        Because she thought the sun
+        <br />
+        Had got no business to be there
+        <br />
+        After the day was done —
+        <br />
+        “It’s very rude of him,” she said,
+        <br />
+        “To come and spoil the fun.”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        The sea was wet as wet could be,
+        <br />
+        The sands were dry as dry.
+        <br />
+        You could not see a cloud, because
+        <br />
+        No cloud was in the sky:
+        <br />
+        No birds were flying overhead —
+        <br />
+        There were no birds to fly.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        The Walrus and the Carpenter
+        <br />
+        Were walking close at hand;
+        <br />
+        They wept like anything to see
+        <br />
+        Such quantities of sand:
+        <br />
+        “If this were only cleared away,”
+        <br />
+        They said, “it would be grand!”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “If seven maids with seven mops
+        <br />
+        Swept it for half a year,
+        <br />
+        Do you suppose,” the Walrus said,
+        <br />
+        “That they could get it clear?”
+        <br />
+        “I doubt it,” said the Carpenter,
+        <br />
+        And shed a bitter tear.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “O Oysters, come and walk with us!”
+        <br />
+        The Walrus did beseech.
+        <br />
+        “A pleasant walk, a pleasant talk,
+        <br />
+        Along the briny beach:
+        <br />
+        We cannot do with more than four,
+        <br />
+        To give a hand to each.”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        The eldest Oyster looked at him,
+        <br />
+        But never a word he said:
+        <br />
+        The eldest Oyster winked his eye,
+        <br />
+        And shook his heavy head —
+        <br />
+        Meaning to say he did not choose
+        <br />
+        To leave the oyster-bed.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        But four young Oysters hurried up,
+        <br />
+        All eager for the treat:
+        <br />
+        Their coats were brushed, their faces washed,
+        <br />
+        Their shoes were clean and neat —
+        <br />
+        And this was odd, because, you know,
+        <br />
+        They hadn’t any feet.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        Four other Oysters followed them,
+        <br />
+        And yet another four;
+        <br />
+        And thick and fast they came at last,
+        <br />
+        And more, and more, and more —
+        <br />
+        All hopping through the frothy waves,
+        <br />
+        And scrambling to the shore.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        The Walrus and the Carpenter
+        <br />
+        Walked on a mile or so,
+        <br />
+        And then they rested on a rock
+        <br />
+        Conveniently low:
+        <br />
+        And all the little Oysters stood
+        <br />
+        And waited in a row.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “The time has come,” the Walrus said,
+        <br />
+        “To talk of many things:
+        <br />
+        Of shoes — and ships — and sealing-wax —
+        <br />
+        Of cabbages — and kings —
+        <br />
+        And why the sea is boiling hot —
+        <br />
+        And whether pigs have wings.”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “But wait a bit,” the Oysters cried,
+        <br />
+        “Before we have our chat;
+        <br />
+        For some of us are out of breath,
+        <br />
+        And all of us are fat!”
+        <br />
+        “No hurry!” said the Carpenter.
+        <br />
+        They thanked him much for that.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “A loaf of bread,” the Walrus said,
+        <br />
+        “Is what we chiefly need:
+        <br />
+        Pepper and vinegar besides
+        <br />
+        Are very good indeed —
+        <br />
+        Now if you’re ready, Oysters dear,
+        <br />
+        We can begin to feed.”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “But not on us!” the Oysters cried,
+        <br />
+        Turning a little blue.
+        <br />
+        “After such kindness, that would be
+        <br />
+        A dismal thing to do!”
+        <br />
+        “The night is fine,” the Walrus said.
+        <br />
+        Do you admire the view?
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “It was so kind of you to come!
+        <br />
+        And you are very nice!”
+        <br />
+        The Carpenter said nothing but
+        <br />
+        “Cut us another slice:
+        <br />
+        I wish you were not quite so deaf —
+        <br />
+        I’ve had to ask you twice!”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “It seems a shame,” the Walrus said,
+        <br />
+        “To play them such a trick,
+        <br />
+        After we’ve brought them out so far,
+        <br />
+        And made them trot so quick!”
+        <br />
+        The Carpenter said nothing but
+        <br />
+        “The butter’s spread too thick!”
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “I weep for you,” the Walrus said:
+        <br />
+        “I deeply sympathize.”
+        <br />
+        With sobs and tears he sorted out
+        <br />
+        Those of the largest size,
+        <br />
+        Holding his pocket-handkerchief
+        <br />
+        Before his streaming eyes.
+      </p>
+      <p style={{ marginTop: '0.5em' }}>
+        “O Oysters,” said the Carpenter,
+        <br />
+        “You’ve had a pleasant run!
+        <br />
+        Shall we be trotting home again?”
+        <br />
+        But answer came there none —
+        <br />
+        And this was scarcely odd, because
+        <br />
+        They’d eaten every one.
+      </p>
+    </>
+  );
 }
