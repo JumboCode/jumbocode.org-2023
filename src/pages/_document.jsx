cd16d045282d5b81ed4f46194c1e44a1a58a07cc
Luke Taylor <luke@deentaylor.com>
2022-02-15 17:41:16 -0500

    Favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,6 +9,15 @@ class CustomDocument extends Document {
           {/* Preload fonts */}
           <link rel="preload" as="font" type="font/woff2" href="/fonts/inter/Inter-roman.var.woff2?v=3.19" crossOrigin="anonymous" />
           <link rel="preload" as="font" type="font/woff2" href="/fonts/inter/Inter-italic.var.woff2?v=3.19" crossOrigin="anonymous" />
+
+          {/* Favicon & friends */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#14ebb9" />
+          <meta name="theme-color" content="#0a0a0c" />
         </Head>
 
         <body>
