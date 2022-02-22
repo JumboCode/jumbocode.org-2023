3b08c16e5270f13f9db918d5efcd1a7ed9e4e3db
Luke Taylor <luke@deentaylor.com>
2022-02-22 13:11:29 -0500

    Circular particles

--- src/components/ParticleCloud/shaders/fragment.glsl
+++ src/components/ParticleCloud/shaders/fragment.glsl
@@ -1,6 +1,13 @@
 precision highp float;
 
+uniform float pointSize;
+
 void main() {
     vec3 color = vec3(20., 235., 185.) / 255.;
-    gl_FragColor = vec4(color, 1.) * 0.1;
+    vec2 st = (gl_PointCoord.xy - 0.5) * pointSize;
+    float radius = pointSize / 2.;
+    float distanceFromCenter = distance(vec2(0), st);
+    float inCircle = 1. - smoothstep(radius - 0.5, radius, distanceFromCenter);
+    float alpha = 0.1 * inCircle;
+    gl_FragColor = vec4(color, 1.) * alpha;
 }
