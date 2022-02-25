319932bcdbf8940ea4c705fbf3d1d828971bdfd9
Xenia <Xenia.Rudchenko@gmail.com>
2022-02-25 16:48:53 -0500

    component set up

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import classNames from 'classnames/bind';
 import styles from './ContactUs.module.scss';
 const cx = classNames.bind(styles);
 
 export default function ContactUs({ className }) {
   return (
-    <>
-      <div className={classNames(cx('base'), className)}>
-        <div className={classNames(cx('contact-col'), className)}>
-          <h2>Lets work together</h2>
-          <h4>
-            Nice, enticing, and exciting description that will making people
-            want to work with us
-          </h4>
-          <button type="button">Contact Us</button>
+    <div className={classNames(cx('base'), className)}>
+      <div className={classNames(cx('contact-col'), className)}>
+        <h2>Lets work together</h2>
+        <h4>
+          Nice, enticing, and exciting description that will making people want
+          to work with us
+        </h4>
+        <button type="button">Contact Us</button>
+      </div>
+      <div className={classNames(cx('groups'), className)}>
+        <div className={classNames(cx('group'), className)}>
+          <img src="../../../public/Buildings.svg" alt="Buildings" />
+          <h5>Organizations</h5>
+          <p>Nice, enticing, and exciting description of working with us</p>
+          <a>Get in touch</a>
+        </div>
+        <div className={classNames(cx('group'), className)}>
+          <img src="../../../public/Student.svg" alt="Student" />
+          <h5>Students</h5>
+          <p>Nice, enticing, and exciting description of working with us</p>
+          <a>Apply</a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
