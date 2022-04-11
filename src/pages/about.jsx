ce1b10d05a8c878437276e4fca8c29c7387b54c0
Luke Taylor <luke@deentaylor.com>
2022-04-11 13:08:16 -0400

    Integrate header into about page with more natural spacing

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import Header from '../components/Header';
 import ContactUs from 'components/ContactUs';
 
+import classNames from 'classnames/bind';
+import styles from './about.module.scss';
+const cx = classNames.bind(styles);
+
 export default function AboutPage() {
   return (
-    <div>
-      <Header />
+    <div className={cx('base')}>
+      <section>
+        <Header />
+      </section>
       <ContactUs />
     </div>
   );
