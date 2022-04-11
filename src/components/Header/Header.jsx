5a8adec5a0ffd3e570f1dca2bfd7b1349525fd5c
Luke Taylor <luke@deentaylor.com>
2022-04-11 13:11:51 -0400

    h1 for SEO

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,10 @@ const cx = classNames.bind(styles);
 export default function Header() {
   return (
     <div className={classNames(cx('base'))}>
-      <h3 className={classNames(cx('text'))}>
+      <h1 className={classNames(cx('text'))}>
         We design, develop, and ship high-quality bespoke software at no cost
         for nonprofit organizations whose missions we believe in.
-      </h3>
+      </h1>
       <div className={classNames(cx('stats'))}>
         <StatBlock number={100} char="%" description={<>Free product<br />development</>} />
         <StatBlock number={40} char="+" description={<>Websites/Apps<br />launched</>} />
