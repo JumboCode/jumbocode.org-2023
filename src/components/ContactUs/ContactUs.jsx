7a1c4a6f27e0c3906b7455f3b84942fd8079604e
Luke Taylor <luke@deentaylor.com>
2022-04-11 12:43:11 -0400

    Responsive layouts for ContactUs section

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -6,32 +6,30 @@ const cx = classNames.bind(styles);
 
 export default function ContactUs() {
   return (
-    <div className={classNames(cx('base'))}>
-      <div className={classNames(cx('contact-col'))}>
+    <div className={cx('base')}>
+      <div className={cx('contact-col')}>
         <h2>Let’s work together</h2>
         <p>
-          Nice, enticing, and exciting description that will making people want
+          Nice, enticing, and exciting description that will make people want
           to work with us
         </p>
-        <button type="button">Contact Us</button>
+        <button type="button" className={cx('button')}>Contact Us</button>
       </div>
-      <div className={classNames(cx('groups'))}>
-        <div className={classNames(cx('group'))}>
-          <h3>Organizations</h3>
-          <p>Nice, enticing, and exciting description of working with us</p>
-          <a>
-            Get in touch&nbsp;
-            <Arrow />
-          </a>
-        </div>
-        <div className={classNames(cx('group'))}>
-          <h3>Students</h3>
-          <p>Nice, enticing, and exciting description of working with us</p>
-          <a>
-            Apply&nbsp;
-            <Arrow />
-          </a>
-        </div>
+      <div className={cx('group')}>
+        <h3>Organizations</h3>
+        <p>Nice, enticing, and exciting description of working with us</p>
+        <a>
+          Get in touch&nbsp;
+          <Arrow />
+        </a>
+      </div>
+      <div className={cx('group')}>
+        <h3>Students</h3>
+        <p>Nice, enticing, and exciting description of working with us</p>
+        <a>
+          Apply&nbsp;
+          <Arrow />
+        </a>
       </div>
     </div>
   );
