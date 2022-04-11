b2c9a2b5a3f8bef1250a5142284adf81cde59277
Luke Taylor <luke@deentaylor.com>
2022-04-11 17:30:50 -0400

    Fix semantic markup

--- src/pages/apply.jsx
+++ src/pages/apply.jsx
@@ -5,42 +5,46 @@ const cx = classNames.bind(styles);
 
 export default function ApplyPage() {
   return (
-    <div className={classNames(cx('base'))}>
-      <div className={classNames(cx('heading'))}>
-        <h2>Learn about the application process</h2>
-      </div>
-      <div className={classNames(cx('list'))}>
-        <ol>
-          <h5>
-            <li>Submit your application</li>
-          </h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-          <h5>
-            <li>Review</li>
-          </h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-          <h5>
-            <li>Decision</li>
-          </h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-          <h5>
-            <li>Onboarding</li>
-          </h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </ol>
-      </div>
+    <div className={cx('base')}>
+      <h2 className={cx('heading')}>Learn about the application process</h2>
+      <ol className={cx('list')}>
+        <li>
+          <h3>Submit your application</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li>
+          <h3>Review</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li>
+          <h3>Decision</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+        <li>
+          <h3>Onboarding</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </li>
+      </ol>
     </div>
   );
 }
