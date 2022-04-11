da2989f03b4895b447fc3e54f6e38c1f1b3e7f37
Luke Taylor <luke@deentaylor.com>
2022-04-11 12:07:49 -0400

    Add stub about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContactUs from 'components/ContactUs';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <ContactUs />
+    </div>
+  );
+}
