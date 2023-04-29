36ee427291b7a9ff51b55fe021dac923131af372
Luke Taylor <luke@deentaylor.com>
2023-04-29 18:43:38 -0400

    color tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0a0a0c" />
-        <meta name="msapplication-TileColor" content="#0a0a0c" />
-        <meta name="theme-color" content="#0a0a0c" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#03000c" />
+        <meta name="msapplication-TileColor" content="#03000c" />
+        <meta name="theme-color" content="#03000c" />
 
         <meta property="og:locale" content="en_US" />
 
