7396cb6f5339b6695e044c4d49926714ba552009
Luke Taylor <luke@deentaylor.com>
2023-04-29 23:54:01 -0400

    Footer polish

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 
+import FacebookLogo from './logos/facebook.svg';
+import GithubLogo from './logos/github.svg';
+import InstagramLogo from './logos/instagram.svg';
+import LinkedinLogo from './logos/linkedin.svg';
+
 import classNames from 'classnames/bind';
 import styles from './Footer.module.scss';
 const cx = classNames.bind(styles);
@@ -9,28 +14,43 @@ const cx = classNames.bind(styles);
 export default function Footer() {
   return (
     <footer className={cx('base')}>
-      <div>
-        <p>JumboCode</p>
-        <a href="https://www.facebook.com/JumboCode/">Facebook</a>
-        <a href="https://www.instagram.com/JumboCode/">Instagram</a>
-        <a href="https://www.linkedin.com/company/tuftsjumbocode">LinkedIn</a>
-      </div>
-      <div>
-        <p>General</p>
+      <div className={cx('column')}>
+        <h2>JumboCode</h2>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
-        <Link href="/work">Work</Link>
-        <Link href="/contact">Contact</Link>
+        <Link href="/work">Our work</Link>
       </div>
-      <div>
-        <p>Students</p>
-        <a href="/roles">Apply</a>
+
+      <div className={cx('column')}>
+        <h2>Students</h2>
+        <a href="/roles">Applications</a>
         <Link href="/roles#faq">FAQ</Link>
       </div>
-      <div>
-        <p>Clients</p>
+
+      <div className={cx('column')}>
+        <h2>Clients</h2>
         <Link href="/contact">Work with us</Link>
-        <Link href="/contact">Contact</Link>
+      </div>
+
+
+      <div className={cx('social-links')}>
+        <a href="https://www.facebook.com/JumboCode/" target="_blank" rel="noopener noreferrer">
+          <FacebookLogo />
+        </a>
+        <a href="https://www.instagram.com/JumboCode/" target="_blank" rel="noopener noreferrer">
+          <InstagramLogo />
+        </a>
+        <a href="https://www.linkedin.com/company/tuftsjumbocode" target="_blank" rel="noopener noreferrer">
+          <LinkedinLogo />
+        </a>
+        <a href="https://github.com/jumbocode" target="_blank" rel="noopener noreferrer">
+          <GithubLogo />
+        </a>
+      </div>
+
+
+      <div className={cx('copyright')}>
+        &copy; {new Date().getUTCFullYear()} JumboCode
       </div>
     </footer>
   );
